b5695df55e60b13b6194e2c77be9d84c5136d314
Masaki Hara <ackie.h.gmai@gmail.com>
2019-10-27 14:56:00 +0900

    Add call tests

--- test/metamarshal_test.rb
+++ test/metamarshal_test.rb
@@ -6,4 +6,19 @@ class MetamarshalTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Metamarshal::VERSION
   end
+
+  def test_parse_call
+    assert_equal 42, ::Metamarshal.parse("\x04\x08i\x2F")
+  end
+
+  def test_parse_module_method
+    klass = Class.new do
+      include ::Metamarshal
+
+      def answer
+        parse("\x04\x08i\x2F")
+      end
+    end
+    assert_equal 42, klass.new.answer
+  end
 end
